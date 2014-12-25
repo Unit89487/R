39ba1e06ca97e638f4500575ca98f400230bac9a
Jay Abdallah <jay.h.abdallah@gmail.com>
2014-12-24 20:32:38 -0500

    Just saving this stuff on here so I don't forget. Would like to have more fun with R later.

---
+++ firstScript.r
@@ -0,0 +1,12 @@
+x <- 10
+y <- x^2 + 5
+z <- y + x
+
+##Intro to R with Data Viz. is a useful online resource.
+##I need a CSV file, need to load that into R with the functions read.table() or
+##read.csv() ]
+
+
+
+## Once you've successfully read it in, use ?plot to in the R console to check out the
+##various plotting capabilities R offers. 
